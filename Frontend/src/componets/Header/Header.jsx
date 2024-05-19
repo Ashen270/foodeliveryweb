d26c24dedf1d53363357b1f1f8765017b94921c2
Ashen Shamilka Wickramasinghe <ashenshamilka270@gmail.com>
2024-05-19 19:10:55 +0530

    header completed

---
+++ Frontend/src/componets/Header/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+       <div className='header-contents'>
+        <h2> Order Your Favourite Food Here</h2>
+        <p>Choose from a diverse menu featuring a delectable arry of dishes craftef  with the finest ingrediants and clinary expertise.
+            Our mission is satisfy your cravings and elevate your dining expirience, one delicious meal at a time.</p>
+       <button>View Menu</button>
+       
+       </div>
+
+    </div>
+  )
+}
+
+export default Header
