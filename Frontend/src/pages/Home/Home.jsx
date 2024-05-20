4871c70fa93142466f9b4b97130990db6feb2cd8
Ashen Shamilka Wickramasinghe <ashenshamilka270@gmail.com>
2024-05-20 19:58:06 +0530

    ExploreMenu Componet Done

--- Frontend/src/pages/Home/Home.jsx
+++ Frontend/src/pages/Home/Home.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import './Home.css'
 import Header from '../../componets/Header/Header'
+import ExploreMenu from '../../componets/ExploreMenu/ExploreMenu'
 
 const Home = () => {
+
+  const [category,setCategory] = React.useState('All')
+
   return (
     <div>
         <Header />
+        <ExploreMenu category={category} setCategory={setCategory}/>
     </div>
   )
 }
