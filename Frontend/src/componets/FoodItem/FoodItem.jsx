c2fce55f2cf6850e36fb8d60065d30813c85a8ff
Ashen Shamilka Wickramasinghe <ashenshamilka270@gmail.com>
2024-05-20 22:39:34 +0530

    fooditem componet done

--- Frontend/src/componets/FoodItem/FoodItem.jsx
+++ Frontend/src/componets/FoodItem/FoodItem.jsx
@@ -3,18 +3,31 @@ import './FoodItem.css'
 import { assets } from '../../assets/assets'
 
 const FoodItem = ({id,name,price,description,image}) => {
+
+      const [itemCount,setItemCount] = React.useState(0)
+
   return (
     <div className='food-item'>
         <div className="food-item-image-container">
             <img src={image} alt="" className="food-item-image" />
+            {
+                !itemCount?
+
+                <img  className="add" onClick={() =>setItemCount(prev=>prev+1)}src={assets.add_icon_white} alt="" />:
+                <div className='food-item-counter'>
+                    <img onClick={()=>setItemCount(prev=>prev-1)} src={assets.remove_icon_red} alt="" />
+                    <p>{itemCount}</p>
+                    <img onClick={()=>setItemCount(prev=>prev+1)} src={assets.add_icon_green} alt="" />
+                </div>
+            }
         </div>
       <div className="food-item-info">
         <div className="food-item-name-rating">
                <p>{name}</p>
               <img src={assets.rating_starts} alt="" />
+        </div>
               <p className='food-item-dec'>{description}</p>
               <p className="food-item-price">${price}</p>
-        </div>
       </div>
     </div>
   )
