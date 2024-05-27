4cc4284f35fc23ba759ddd59b73ce502862df4fc
Ashen Shamilka Wickramasinghe <ashenshamilka270@gmail.com>
2024-05-27 06:58:59 +0530

    create the all APIs to create, list and remove the food items

--- Backend/controllers/foodController.js
+++ Backend/controllers/foodController.js
@@ -42,6 +42,17 @@ const listFood = async (req, res) => {
 
 // remove food item
 
+const  removeFood = async (req, res) => {
+    try {
+        const food = await foodModel.findById(req.body.id);
+        fs.unlink(`uploads/${food.image}`,() => {});
 
+        await foodModel.findByIdAndDelete(req.body.id);
+        res.json({success:true, message:"Food Item Removed Successfully"})
+    } catch (error) {
+        console.log(error);
+        res.json({success:false, message:"Failed to remove food item"}) 
+    }
+}
 
-export {addFood,listFood}
+export {addFood,listFood,removeFood}
