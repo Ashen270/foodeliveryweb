39ecc52655c99fedaa1aa9479c8cc5d3775b48f5
Ashen Shamilka Wickramasinghe <ashenshamilka270@gmail.com>
2024-06-01 02:32:16 +0530

    tomato to #40A578

--- Frontend/src/componets/Header/Header.jsx
+++ Frontend/src/componets/Header/Header.jsx
@@ -1,19 +1,29 @@
-import React from 'react'
-import './Header.css'
+import React from 'react';
+import { assets } from '../../assets/assets';  // Adjust the path as needed
+import './Header.css';
 
 const Header = () => {
   return (
-    <div className='header'>
-       <div className='header-contents'>
-        <h2> Order Your Favourite Food Here</h2>
-        <p>Choose from a diverse menu featuring a delectable arry of dishes craftef  with the finest ingrediants and clinary expertise.
-            Our mission is satisfy your cravings and elevate your dining expirience, one delicious meal at a time.</p>
-       <button>View Menu</button>
-       
-       </div>
-
+    <div
+      className="header"
+      style={{
+        backgroundImage: `url(${assets.header_img})`,
+        backgroundSize: 'contain',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+      }}
+    >
+      <div className="header-contents">
+        <h2>Order Your Favourite Food Here</h2>
+        <p>
+          Choose from a diverse menu featuring a delectable array of dishes crafted
+          with the finest ingredients and culinary expertise. Our mission is to satisfy
+          your cravings and elevate your dining experience, one delicious meal at a time.
+        </p>
+        <button>View Menu</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
