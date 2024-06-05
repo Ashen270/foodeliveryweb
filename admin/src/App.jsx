bedc0ad916812ed0a7e2c8e5af6797a2161ccd97
Ashen Shamilka Wickramasinghe <ashenshamilka270@gmail.com>
2024-06-05 22:09:41 +0530

    admin

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import Navbar from './componets/Navbar/Navbar'
+import Sidebar from './componets/Sidebar/Sidebar'
 
 const App = () => {
   return (
     <div>
       
+             <Navbar />
+             <hr />
+             <div className="app-content">
+              <Sidebar />
+             </div>
+
+
     </div>
   )
 }
---
+++ admin/src/componets/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Navbar.css'
+import {assets} from '../../assets/assets'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <img className='logo' src={assets.logo} alt="" />
+        <img src="" alt="" />
+      
+
+    </div>
+  )
+}
+
+export default Navbar
