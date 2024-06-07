9198e21ceba8c178a59aafa9e369dc272b3b6a40
Ashen Shamilka Wickramasinghe <ashenshamilka270@gmail.com>
2024-06-07 10:30:37 +0530

    admin/add item done

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import Navbar from './componets/Navbar/Navbar'
 import Sidebar from './componets/Sidebar/Sidebar'
+import { Route, Routes } from 'react-router-dom'
+import Add from './Pages/Add/Add'
+import List from './Pages/List/List'
+import Orders from './Pages/Orders/Orders'
 
 const App = () => {
   return (
@@ -10,6 +14,11 @@ const App = () => {
              <hr />
              <div className="app-content">
               <Sidebar />
+              <Routes>
+                <Route path="/add" element={<Add/>} />
+                <Route path="/list" element={<List/>} />
+                <Route path="/orders" element={<Orders/>} />
+              </Routes>
              </div>
 
 
